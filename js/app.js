cbecd49621069a6c0790616ce3c79a8b4b166481
rbidon <roussebidon@gmail.com>
2022-07-13 04:31:39 -0400

    Created two redirection for the start page

--- js/app.js
+++ js/app.js
@@ -1,2 +1,40 @@
+let playerOneName = "";
+let playerTwoName = "";
 //1. Grab two player button to initiate the prompt a then the start button 
-let twoPlayerBtn = document.querySelector("twoPlayer");
+let twoPlayerBtn = document.querySelector(".twoPlayer");
+twoPlayerBtn.addEventListener("click", (e) =>{
+    e.preventDefault();
+    alert("Welcome to Matchy!");
+    // Prompt for Player One Name
+    while(playerOneName === ""){
+        playerOneName = prompt("What is your name, Player One?");
+    }console.log(playerOneName);
+    // Prompt for Player Two
+    while(playerTwoName === ""){
+        playerTwoName = prompt("What is your name, Player Two?");
+    }console.log(playerTwoName);
+
+    // Choosing Difficulity: Easy Road/ Hard Road
+     let chooseDifficulty = ""; 
+     while(chooseDifficulty === ""){
+        chooseDifficulty = prompt(`Hello ${playerOneName} & ${playerTwoName}. Choose your Difficulty Easy Road or Hard Road?`)
+    };
+     console.log(`Players have selected the ${chooseDifficulty}`);
+     // Player Select Difficulity
+     if(chooseDifficulty === "Easy" || "Easy Road"){
+        // direct them to the start page for Easy Road
+        // window.location.assign("../startEasy.html");
+        // Will display the change screen to the start button
+        document.querySelector("#homepageBackground").style.display = "none";
+        document.querySelector("#startPage").style.display = "flex";
+        document.querySelector("#startPage").style.justifyContent = "center";
+        document.querySelector("#startPage").style.alignItems ="center";
+        document.querySelector(".instructionSection").style.display = "flex";
+        document.querySelector(".instructionSection").style.flexDirection = "column";
+        document.querySelector(".instructionSection").style.justifyContent = "flex-start";
+
+        
+     }
+});
+//2. Start Button & Card Div
+let startButton = document.querySelector(".start");
